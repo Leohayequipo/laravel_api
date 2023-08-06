b1eb314460a3aebcbcff81e1816bfd3f513d6622
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-06 16:36:44 -0300

    Maquetado-info de base y paginacion

--- api/resources/views/index.blade.php
+++ api/resources/views/index.blade.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <script src="https://cdn.tailwindcss.com"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
     </head>
-    <body class="">
-       @foreach($posts as $post)
-        <div>
-            <h2>{{$post->title}}</h2>
+    <body class="bg-gray-100 text-gray-700">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-3 my-10">
+                @foreach($posts as $post)
+                    <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                        <h2 class="font-bold text-lg mb-4">{{$post->title}}</h2>
+                        <p class="text-xs">{{$post->excerpt}}</p>
+                        <p class="text-xs text-right">{{$post->published_at}}</p>
+
+                    </div>
+                @endforeach
+            </div>
+            <div class="mb-10">
+                <!-- la paginacion viene lista con el metodo links (en el controlador PostController esta el metodo paginate)-->
+                {{ $posts->links() }}
+            </div>
+       
         </div>
-       @endforeach
-        <!-- la paginacion viene lista con el metodo links (en el controlador PostController esta el metodo paginate)-->
+      
         
-        {{ $posts->links() }}
     </body>
 </html>
