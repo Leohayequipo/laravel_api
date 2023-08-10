37b89f6f77f76be7bbe2bcb06433873f4053a57e
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-10 10:00:47 -0300

    Api login

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,10 +10,19 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
     ->only(['index','show','destroy']);*/
 
 //v1
+//proteccion middleware
+
 Route::apiResource('v1/posts',PostV1::class)
-    ->only(['index','show','destroy']);
+    ->only(['index','show','destroy'])
+    ->middleware('auth:sanctum');
 
 //V2
 Route::apiResource('v2/posts',PostV2::class)
-    ->only(['index','show']);
+    ->only(['index','show'])
+    ->middleware('auth:sanctum');
+
+Route::post('login',[
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
 
